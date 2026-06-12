2ae32f809aa86f52ebc09514e89c05a77c1dfd73
royardra58@gmail.com <royardra58@gmail.com>
2026-06-12 10:02:37 +0530

    initial commit with python basics

---
+++ python_basics.py
@@ -0,0 +1,20 @@
+""""
+name=(input("enter your name : "))
+age=int(input("enter your age : "))
+print(f"my name is{name} i am {age} years old")
+
+"""""
+""""
+a=int(input("enter five numbers : "))
+b=int(input("enter two numbers : "))
+print(int(a+b))
+"""
+"""
+d=[1,2,3]
+d=d+["numbers"]
+print(d)
+"""
+print("hello"*5)
+
+
+
---
+++ typecasting.py
@@ -0,0 +1,10 @@
+
+a=int("21")
+print(type(a))
+
+b=str(21)
+print(type(b))
+
+fruit="apple"
+print(list(fruit))
+
