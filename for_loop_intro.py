743136c50a150e9c737f89cadeddcb32019e6d62
royardra58@gmail.com <royardra58@gmail.com>
2026-06-15 11:16:48 +0530

    Initial commit

---
+++ for_loop_intro.py
@@ -0,0 +1,42 @@
+"""
+for k in "python":
+    print(k)
+"""
+"""
+for c in (1,2,3):
+    print(c)
+"""
+"""
+for k in range(5):
+    print(k)
+"""
+"""
+for k in range(0,9):
+    print(k)
+"""
+"""
+for k in range(10,5,-1):
+    print(k)    
+"""   
+"""num=int(input("enter the number:"))
+    fact=5
+print(num)
+"""
+'''num=[45,12,22,23,4,8,34]
+for j in num:
+    print(j)
+'''
+'''
+count=int(input("enter how many number you wish to enter:"))
+numbers=[]
+for k in range(1,count+1):
+    num=int(input(f"{k},enter the number : "))
+    numbers=numbers+[num]
+print(numbers)    
+'''
+
+n=int(input("enter the number : "))
+for h in  range(0,9):
+ if h%2==0:
+ print(n) 
+ 
