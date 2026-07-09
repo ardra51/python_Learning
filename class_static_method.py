5eb25a84bb918cab52986a59d22d202dbb45c971
ardra1111 <royardra58@gmail.com>
2026-07-09 11:20:38 +0530

    pattern

---
+++ class_static_method.py
@@ -0,0 +1,15 @@
+'''class message:
+    @staticmethod
+    def greet():
+        print("hello")
+message.greet()'''
+
+'''
+class student:
+    course="python"
+    @classmethod
+    def change(cls):
+        cls.course="java"
+student.change()
+print(student.course)'''
+
--- sample_ prgm.py
+++ sample_ prgm.py
@@ -6,9 +6,26 @@ for k in range(1,6):
         num+=1
     print()
 '''
-
+'''
 for k in range(1,6):
     for c in range(k,0,-1):
         print(c,end=" ")
         
-    print()
+    print()'''
+
+
+
+ #for i in range(1,5):
+ #  print(" *"*i)
+
+
+'''for i in range(4,1,-1):
+    print(" *" *i)'''
+
+'''for r in range(1,6):
+    for sp in range(5-r):
+        print(" ",end="")
+    print("*"*r)'''  
+
+for r in range(1,6):
+    print(" "*int(5-r),"*"*r)
